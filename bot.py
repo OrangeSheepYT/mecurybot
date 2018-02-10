506b4f05a70bc5c8a8f88c7c0ad25ab8d731db94
OrangeSheepYT <mattmarkdonnelly@gmail.com>
2018-02-10 10:52:46 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,4 +143,4 @@ if __name__ == "__main__":
             exc = '{}: {}'.format(type(e).__name__, e)
             print('Failed to load extension {}\n'.format(extension))
 
-bot.run('')
+bot.run(process.env.BOT_TOKEN)
